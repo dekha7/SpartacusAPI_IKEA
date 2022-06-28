7ab571d4e361b53acffd6a9a3cf09aa22f684034
Deepak  Khare <deepak.khare@ingka.com>
2022-06-28 12:31:34 +0200

    Modified description in swagger file. and modified connectonstring.

--- DbConnection.cs
+++ DbConnection.cs
@@ -8,7 +8,7 @@ namespace SpartacusAPI
 {
     public static class DbConnection
     {
-<<<<<<< HEAD
+
         public static string ConnectionString
         {
             get
@@ -17,15 +17,6 @@ namespace SpartacusAPI
                 //return "Data Source=CFPSEELM-NT2027.ikeadt.com;Initial Catalog=CFSQL0051;User ID=SPARTACUS_DEV;Password=Spartacusd3v;Connection Timeout=120;";
             }
         }
-=======
-        //public static string ConnectionString
-        //{
-        //    get
-        //    {
-        //        //return "Data Source=PTPSEELM-NT2069.ikeadt.com;Initial Catalog=PTSQL0172;User ID=SPARTACUS_DEV;Password=sparepli@*!VDS98;Connection Timeout=120;";
-        //        return "Data Source=CFPSEELM-NT2027.ikeadt.com;Initial Catalog=CFSQL0051;User ID=SPARTACUS_DEV;Password=Spartacusd3v;Connection Timeout=120;";
-        //    }
-        //}
->>>>>>> ff18b9c3ce748adbb89d2b1c0fd9146856c259f0
+
     }
 }
